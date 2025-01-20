51d1b266dfdb83cba02161e546b52f0999a3fff6
Rodrigo Camilo <digo-camilo2@hotmail.com>
2025-01-20 19:52:13 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -20,24 +20,28 @@ public class Game {
 	
 	@Column(name = "game_year")
 	private Integer year;
-	private String gnre;
-	private String patforms;
+	private String genre;
+	private String platforms;
 	private Double score;	
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+		
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String gnre, String patforms, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
-		this.gnre = gnre;
-		this.patforms = patforms;
+		this.genre = genre;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -69,19 +73,19 @@ public class Game {
 	}
 
 	public String getGnre() {
-		return gnre;
+		return genre;
 	}
 
 	public void setGnre(String gnre) {
-		this.gnre = gnre;
+		this.genre = gnre;
 	}
 
 	public String getPatforms() {
-		return patforms;
+		return platforms;
 	}
 
 	public void setPatforms(String patforms) {
-		this.patforms = patforms;
+		this.platforms = patforms;
 	}
 
 	public Double getScore() {
